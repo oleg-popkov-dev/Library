c2ad1a185e83a56cf637a2d02319ea598e1ea9c0
Oleg Popkov <oleg.popkov.dev@gmail.com>
2021-07-22 08:50:38 +0600

    practice

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        UserAdministrator userAdministrator = new UserAdministrator("Oлег");
+        SupplierReader supplierReader = new SupplierReader("Катя");
+        LibrarianAdministrator librarianAdministrator = new LibrarianAdministrator("Вася");
+
+        userAdministrator.overdueNotification(supplierReader);
+        supplierReader.takeBook(userAdministrator);
+        supplierReader.returnBook(userAdministrator);
+        librarianAdministrator.orderBook(supplierReader);
+    }
+}
---
+++ src/UserAdministrator.java
@@ -0,0 +1,10 @@
+public class UserAdministrator extends User implements Administrator {
+
+    public UserAdministrator(String name) {
+        super(name);
+    }
+
+    public void overdueNotification(Reader reader) {
+        System.out.println("Администратор " + name + " обнаружил просрочку возврата книги у пользователя " + reader);
+    }
+}
